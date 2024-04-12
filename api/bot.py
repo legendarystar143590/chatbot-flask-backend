af6e7191899cb9045f2211f48f50c44db8ae7f98
Buzuev1234 <buzuevaleksandr71@gmail.com>
2024-04-12 23:10:17 +0200

    updated create_bot api

--- api/bot.py
+++ api/bot.py
@@ -1,5 +1,30 @@
 from flask import Blueprint, request, jsonify
+from models import Bot
+import os
 
 
 
 bot_blueprint = Blueprint('bot_blueprint', __name__)
+
+@bot_blueprint.route('/create_bot', methods=['POST'])
+def create_bot():
+    try:
+        dat = request.form
+        name = data['name']
+        avatar = request.files['avatar']
+        color = data['color'] 
+        active = data['active'] 
+        start_time = data['start_time'] 
+        end_time = data['end_time'] 
+        knowledge_base = data['knowledge_base']
+        
+        if avatar:
+            filename = secure_file(avatar.filename)
+            avatar.save(os.path.join(app.config['UPLOAD_FOLDER']))
+        new_bot = Bot(name=name, avatar=filename, color=color, active=active, start_time=start_time, end_time=end_time, knowledge_base=knowledge_base)
+        new_bot.save()
+
+        return jsonify({'message': 'Success'}), 201
+    except:
+        return jsonify({'error':'Server is busy!'}), 500
+
